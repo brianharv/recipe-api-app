8d99499834019756d97caed69d2b9376136c1de9
Brian Harvey <brianpaulharvey@gmail.com>
2026-07-17 08:13:44 -0700

    fix linting errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,8 +1,10 @@
 """ Calculator functions """
 
+
 def add(x, y):
     """Return the sum of the 2 parameters"""
     return x + y
+
 
 def subtract(x, y):
     """Subract x from y and return the difference"""
--- app/app/tests.py
+++ app/app/tests.py
@@ -2,6 +2,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
 	def test_add_two_numbers(self):
 		res = calc.add(4, 7)
