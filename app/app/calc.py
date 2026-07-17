92ecdb0fb7dcb5dcea0183b9ec5ab19110e1edc9
Brian Harvey <brianpaulharvey@gmail.com>
2026-07-17 08:07:17 -0700

    adds test files and postgres

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+""" Calculator functions """
+
+def add(x, y):
+    """Return the sum of the 2 parameters"""
+    return x + y
+
+def subtract(x, y):
+    """Subract x from y and return the difference"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+	def test_add_two_numbers(self):
+		res = calc.add(4, 7)
+    
+		self.assertEqual(res, 11)
+
+	def test_subtract_numbers(self):
+		res = calc.subtract(10, 15)
+
+		self.assertEqual(res, 5)
