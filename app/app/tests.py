7720378aa4911a34c287fd9ba0a68e7a72445dc5
Brian Harvey <brianpaulharvey@gmail.com>
2026-07-17 11:21:34 -0700

    fix linting errors again

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,12 +4,12 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-	def test_add_two_numbers(self):
-		res = calc.add(4, 7)
-    
-		self.assertEqual(res, 11)
+    def test_add_two_numbers(self):
+        res = calc.add(4, 7)
 
-	def test_subtract_numbers(self):
-		res = calc.subtract(10, 15)
+        self.assertEqual(res, 11)
 
-		self.assertEqual(res, 5)
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
